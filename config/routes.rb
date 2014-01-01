11cb20d3aea45198baa1e778576e2fb591ef556f
tsutomu_yamanaka <tsutomy0816@hotmail.com>
2014-01-01 22:31:31 +0900

    prepare initial website

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Asagao::Application.routes.draw do
+  get "top/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  get "about" => "top#about", as: "about"
   
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -49,6 +52,7 @@ Asagao::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+    root to: "top#index"
 
   # See how all your routes lay out with "rake routes"
 
